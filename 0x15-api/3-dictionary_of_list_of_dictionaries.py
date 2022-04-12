6a9a03c204f3461011de34b0ed46f83babb6d812
avocadoclouds <rehana447@gmail.com>
2022-04-12 04:39:20 +0300

    task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Exports data in the JSON format"""
+
+if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
+
+    API = "https://jsonplaceholder.typicode.com"
+
+    #userId = sys.argv[1]
+    users = requests.get('{}/users/'.format(API)).json()
+    userTodo = requests.get('{}/todos'.format(API)).json()
+    userTodoData = {}
+
+    for user in users:
+        taskList = []
+        for task in userTodo:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        userTodoData[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(userTodoData, f)
